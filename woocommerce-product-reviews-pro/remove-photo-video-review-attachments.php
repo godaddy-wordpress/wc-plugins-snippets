799c6f7e0219cb30d2d07c621aafbbfe9a0a5978
Beka Rice <beka.rice@gmail.com>
2016-03-15 12:32:44 -0400

    Product Reviews Pro: remove review attachments

---
+++ woocommerce-product-reviews-pro/remove-photo-video-review-attachments.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Removes the photo and video attachments from Product Reviews Pro review form
+ *
+ * @param array $fields the default contribution fields
+ * @return array the updated fields
+ */
+function sv_wc_product_reviews_pro_remove_review_attachments( $fields ){
+	return $fields[ 'attachment_type' ] = [];
+}
+add_filter( 'wc_product_reviews_pro_default_fields' , 'sv_wc_product_reviews_pro_remove_review_attachments' );
