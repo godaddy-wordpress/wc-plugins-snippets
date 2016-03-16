2904e1b0aea7357138e7e062b580f7be56babd02
Beka Rice <beka.rice@gmail.com>
2016-03-16 18:37:41 -0400

    PIP: snippet to show order delivery date
    …in invoices / packing lists shipping section

---
+++ woocommerce-print-invoices-packing-lists/add-order-delivery-date-to-shipping.php
@@ -0,0 +1,30 @@
+<?php // only copy this line if needed
+/**
+ * Adds Order Delivery date to the "Shipping Method" section of invoices and packing lists
+ * 'Show shipping method' should be enabled for invoices!
+ *
+ * @param string $shipping the shipping method text
+ * @param string $document_type the type of document being viewed
+ * @param \WC_Order $order the order object the document is for
+ * @return string the updated shipping string
+ */
+function sv_wc_pip_add_order_delivery_shipping( $shipping, $document_type, $order ) {
+
+	// if you want to only add this to invoices, you can add another check for document type
+	// if ( 'invoice' !== $document_type ) { return $shipping; }
+
+	// bail if Order Delivery plugin is not active
+	if ( ! function_exists( 'WC_OD' ) ) {
+		return $shipping;
+	}
+
+	$delivery_date = get_post_meta( $order->id, '_delivery_date', true );
+
+	if ( $delivery_date ) {
+		$delivery_date_i18n = wc_od_localize_date( $delivery_date );
+		$shipping .= '<p>We will try our best to deliver your order on: ' . $delivery_date_i18n . '</p>';
+	}
+	
+	return $shipping;
+}
+add_filter( 'wc_pip_shipping_method', 'sv_wc_pip_add_order_delivery_shipping', 10, 3 );
