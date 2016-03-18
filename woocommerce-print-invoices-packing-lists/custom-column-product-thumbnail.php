c8ec8e20ab47250d69c8e0ad77ae863e2855040f
Tamara Zuk <tamarazuk@gmail.com>
2016-03-18 02:13:26 -0400

    PIP: Product thumbnail custom column
    This code snippet adds a "Thumbnail" column to all document types and
    sets it as the first column of the table
    
    The thumbnail reflects the selected variation. If no thumbnail exists,
    the standard WooCommerce placeholder is shown.
    
    Here's an example:
    http://cloud.skyver.ge/2837440O3s1Z

---
+++ woocommerce-print-invoices-packing-lists/custom-column-product-thumbnail.php
@@ -0,0 +1,53 @@
+<?php // only copy this line if needed
+
+/**
+ * Filter the document table headers to add a product thumbnail header
+ *
+ * @param array $table_headers Table column headers
+ * @param int $order_id WC_Order id
+ * @param string $document_type WC_PIP_Document type
+ * @return array The updated table column headers
+ */
+function sv_wc_pip_document_table_headers_product_thumbnail( $table_headers ) {
+
+	$thumbnail_header = array( 'product_thumbnail' => 'Thumbnail' );
+
+	// add product thumnail column as the first column
+	return array_merge( $thumbnail_header, $table_headers );
+}
+add_filter( 'wc_pip_document_table_headers', 'sv_wc_pip_document_table_headers_product_thumbnail' );
+
+
+/**
+ * Filter the document table row cells to add product thumbnail column data
+ *
+ * @param string $table_row_cells The table row cells.
+ * @param string $type WC_PIP_Document type
+ * @param string $item_id Item id
+ * @param array $item Item data
+ * @param \WC_Product $product Product object
+ * @param \WC_Order $order Order object
+ * @return array The filtered table row cells.
+ */
+function sv_wc_pip_document_table_row_cells_product_thumbnail( $table_row_cells, $document_type, $item_id, $item, $product, $order ) {
+
+	// get the product's or variation's thumbnail 'shop_thumbnail' size; we will use CSS to set the width
+	$thumbnail_content = array( 'product_thumbnail' => $product->get_image() );
+
+	// add product thumnail column as the first column
+	return array_merge( $thumbnail_content, $table_row_cells );
+}
+add_filter( 'wc_pip_document_table_row_cells', 'sv_wc_pip_document_table_row_cells_product_thumbnail', 10, 6 );
+
+
+/**
+ * Add custom CSS to set the thumbnail's width
+ */
+function sv_wc_pip_styles_product_thumbnail() {
+
+	echo 'td.product_thumbnail img {
+		width: 75px;
+		height: auto;
+	}';
+}
+add_action( 'wc_pip_styles', 'sv_wc_pip_styles_product_thumbnail' );
