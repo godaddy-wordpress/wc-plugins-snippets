ff35e05c21f80df9ddc67c8915bf02b91b768f9b
Beka Rice <beka.rice@gmail.com>
2016-05-17 23:55:28 -0400

    PIP: Adjust removing columns example

--- woocommerce-print-invoices-packing-lists/remove-columns.php
+++ woocommerce-print-invoices-packing-lists/remove-columns.php
@@ -11,20 +11,19 @@
 function sv_wc_pip_document_table_headers_remove_columns( $table_headers, $order_id, $type ) {
 
 	// remove SKU columns on invoices
-	if ( 'invoice' === $type ) {
-
+	if ( 'invoice' === $type && isset( $table_headers['sku'] ) ) {
 		unset( $table_headers['sku'] );
 	}
 
 	// remove weight column from packing and pick lists
-	elseif ( 'packing-list' === $type || 'pick-list' === $type ) {
-
+	elseif ( ( 'packing-list' === $type || 'pick-list' === $type ) && isset( $table_headers['weight'] ) ) {
 		unset( $table_headers['weight'] );
 	}
 
 	return $table_headers;
 }
 add_filter( 'wc_pip_document_table_headers', 'sv_wc_pip_document_table_headers_remove_columns', 10, 3 );
+
 
 /**
  * Filter the document table row cells to remove column data
@@ -40,17 +39,37 @@ add_filter( 'wc_pip_document_table_headers', 'sv_wc_pip_document_table_headers_r
 function sv_wc_pip_document_table_row_cells_remove_columns( $table_row_cells, $type, $item_id, $item, $product, $order ) {
 
 	// remove SKU columns on invoices
-	if ( 'invoice' === $type ) {
-
+	if ( 'invoice' === $type && isset( $table_row_cells['sku'] ) ) {
 		unset( $table_row_cells['sku'] );
 	}
 
 	// remove weight column from packing and pick lists
-	elseif ( 'packing-list' === $type || 'pick-list' === $type ) {
-
+	elseif ( ( 'packing-list' === $type || 'pick-list' === $type ) && isset( $table_row_cells['weight'] ) ) {
 		unset( $table_row_cells['weight'] );
 	}
 
 	return $table_row_cells;
 }
 add_filter( 'wc_pip_document_table_row_cells', 'sv_wc_pip_document_table_row_cells_remove_columns', 10, 6 );
+
+
+/**
+ * Filter the footer rows to remove data for packing lists
+ *
+ * @param array $rows footer rows and cells
+ * @param string $document_type the type of document being generated
+ * @return array - the updated footer rows and cells
+ */
+function sv_wc_pip_document_table_footer_modification( $rows, $document_type ) {
+
+	if ( 'packing-list' !== $document_type ) {
+		return $rows;
+	}
+
+	if ( isset( $rows['totals']['total-weight'] ) ) {
+		unset( $rows['totals']['total-weight'] );
+	}
+
+	return $rows;
+}
+add_filter( 'wc_pip_document_table_footer', 'sv_wc_pip_document_table_footer_modification', 10, 2 );
