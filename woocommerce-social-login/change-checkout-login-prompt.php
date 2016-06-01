b22b1bec213ee54f31efe0f5c727f1b96c01f40b
Beka Rice <beka.rice@gmail.com>
2016-06-01 12:10:11 -0400

    Social Login: change "checkout" login text in notice

---
+++ woocommerce-social-login/change-checkout-login-prompt.php
@@ -0,0 +1,30 @@
+<?php // only copy this line if needed
+
+/**
+ * Changes "Checkout" notice text to be different from settings 
+ *
+ * Example: http://cloud.skyver.ge/290K3X2T0x1M
+ *
+ * @param string $login_text original login text
+ * @return string - updated login text depending on settings
+ */
+function sv_wc_social_login_change_notice_text( $login_text ) {
+
+	// bail unless we're at checkout and using a "checkout" notice
+	if ( ! ( function_exists( 'is_checkout' ) && is_checkout() && in_array( 'checkout', get_option( 'wc_social_login_display' ), true ) ) ) {
+		return $login_text;
+	}
+		
+	// set a flag so we only update the first appearance of the login text
+ 	static $updated = false;
+ 	
+ 	if ( ! $updated ) {
+ 	
+ 		// update the Social Login text for the checkout notice
+ 		$login_text = 'Want to save time by using a social network?';
+ 		$updated = true;
+ 	}
+ 	
+ 	return $login_text;
+}
+add_filter( 'pre_option_wc_social_login_text', 'sv_wc_social_login_change_notice_text', 15 );
