5021fb14a8baa83f69d6bdfdbfc15d3da8c6142e
Tamara Zuk <tamarazuk@gmail.com>
2016-06-28 23:20:43 -0400

    CSV Export: VAT number column

---
+++ woocommerce-customer-order-csv-export/add-vat-number-to-order-export.php
@@ -0,0 +1,67 @@
+<?php // only copy this line if needed
+
+/**
+ * Add `vat_number` column header
+ *
+ * @param array $column_headers the original column headers
+ * @param \WC_Customer_Order_CSV_Export_Generator $csv_generator the generator instance
+ * @return array the updated column headers
+ */
+function sv_wc_csv_export_modify_column_headers_vat_number( $column_headers, $csv_generator ) {
+
+	$column_headers['vat_number'] = 'vat_number';
+
+	return $column_headers;
+}
+add_filter( 'wc_customer_order_csv_export_order_headers', 'sv_wc_csv_export_modify_column_headers_vat_number', 10, 2 );
+
+/**
+ * Add `vat_number` column data
+ *
+ * this function searches for a VAT number order meta key used by the more
+ * popular Tax/VAT plugins
+ *
+ * @param array $order_data the original column data
+ * @param \WC_Order $order the order object
+ * @param \WC_Customer_Order_CSV_Export_Generator $csv_generator the generator instance
+ * @return array the updated column data
+ */
+function wc_csv_export_modify_row_data_vat_number( $order_data, $order, $csv_generator ) {
+
+	$vat_number = '';
+
+	// find VAT number if one exists for the order
+	$vat_number_meta_keys = array(
+		'VAT Number',
+		'vat_number',                // Taxamo
+		'_billing_wc_avatax_vat_id', // AvaTax
+	);
+
+	foreach ( $vat_number_meta_keys as $meta_key ) {
+
+		if ( metadata_exists( 'post', $order->id, $meta_key ) ) {
+			$vat_number = get_post_meta( $order->id, $meta_key, true );
+			break;
+		}
+	}
+
+	$custom_data = array(
+		'vat_number' => $vat_number,
+	);
+
+	$new_order_data = array();
+
+	if ( isset( $csv_generator->order_format ) && ( 'default_one_row_per_item' == $csv_generator->order_format || 'legacy_one_row_per_item' == $csv_generator->order_format ) ) {
+
+		foreach ( $order_data as $data ) {
+			$new_order_data[] = array_merge( (array) $data, $custom_data );
+		}
+
+	} else {
+
+		$new_order_data = array_merge( $order_data, $custom_data );
+	}
+
+	return $new_order_data;
+}
+add_filter( 'wc_customer_order_csv_export_order_row', 'wc_csv_export_modify_row_data_vat_number', 10, 3 );
