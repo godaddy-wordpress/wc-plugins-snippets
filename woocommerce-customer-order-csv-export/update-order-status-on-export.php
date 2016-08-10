e5d6964ebd0fe876e044f8b940977aeb3106457d
Beka Rice <beka.rice@gmail.com>
2016-08-10 01:20:33 -0400

    CSV Export: update auto-complete snippet...
    with option to only do so if paid

--- woocommerce-customer-order-csv-export/update-order-status-on-export.php
+++ woocommerce-customer-order-csv-export/update-order-status-on-export.php
@@ -9,10 +9,29 @@
 function sv_wc_csv_export_update_exported_order_status( $order, $export_method ) {
 
 	// uncomment this to restrict the order status update to a specific export method
-	//if ( 'ftp' !== $export_method ) {
-	//	return;
-	//}
+	// if ( 'ftp' !== $export_method ) {
+	//	 return;
+	// }
 
-	$order->update_status( 'completed' );
+	$order->update_status( 'completed', 'Order exported to CSV.' );
 }
 add_action( 'wc_customer_order_csv_export_order_exported', 'wc_csv_export_update_exported_order_status', 10, 2 );
+
+
+// OR
+
+
+/**
+ * Update an order's status to completed upon export only if it's paid
+ * Requires WC 2.5+
+ *
+ * @param object $order the \WC_Order object
+ * @param string $export_method the selected export method
+ */
+function sv_wc_csv_export_update_exported_order_status( $order ) {
+
+	if ( $order->is_paid() ) {
+		$order->update_status( 'completed', 'Order exported to CSV.' );
+	}
+}
+add_action( 'wc_customer_order_csv_export_order_exported', 'wc_csv_export_update_exported_order_status' );
