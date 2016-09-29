8d0c5a202ef6884ce1d76a48ce6d661e109e115a
Beka Rice <beka.rice@gmail.com>
2016-09-29 14:23:45 -0400

    CSV Export: update roles in customer export snippet

--- woocommerce-customer-order-csv-export/include-specific-roles-in-customer-export.php
+++ woocommerce-customer-order-csv-export/include-specific-roles-in-customer-export.php
@@ -1,15 +1,17 @@
 <?php // only copy if needed
 
 /**
- * Add additional roles to the customer export
+ * Include specific roles in the customer export instead of all non-employees
+ * REQUIRES WP 4.4+
  *
  * @param array $query_args the arguments for the get_users query
  * @return array
  */
 function sv_wc_csv_export_add_roles_to_customer_export( $query_args ) {
 
-	// we have to use "role__in" instead so a user isn't required to have *all* roles
+	// make sure our snippet is entirely controlling the included roles
 	unset( $query_args['role'] );
+	unset( $query_args['role__not_in'] );
 
 	// add an array of roles to check for; users must have one to be included
 	$query_args['role__in'] = array( 'customer', 'wholesale-customer', 'editor' );
