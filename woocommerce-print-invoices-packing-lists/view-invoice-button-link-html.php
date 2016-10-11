df7eb87981c5fc4e44a8250fb2059915ecf3f748
Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
2016-10-11 03:39:37 -0500

    Create view-invoice-button-link-html.php

---
+++ woocommerce-print-invoices-packing-lists/view-invoice-button-link-html.php
@@ -0,0 +1,22 @@
+<?php // only copy this line if needed
+
+/**
+ * Remove "View Invoice" link from customer emails 
+ * or change HTML of the original link button in "View Order" page from "My Account"
+ *
+ * @param string $button The button HTML
+ * @param string $action The current context where the link button is produced ('send_email' or 'print')
+ * @return string HTML
+ */
+function sv_pip_view_invoice_button_html( $button, $action ) {
+    
+    if ( 'send_email' === $action ) {
+        $button = '';
+    } elseif ( 'print' === $action ) {
+		$button = '<span class="my-css-class">' . $button . '</span>';
+	}
+    
+    return $button;
+}
+
+add_filter( 'wc_pip_view_invoice_button_html', 'sv_pip_view_invoice_button_html', 10, 2 );
