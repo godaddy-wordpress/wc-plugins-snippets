97d9dd375f5001de74208ddb4a26cecf46b50160
Tamara Zuk <tamarazuk@gmail.com>
2016-10-13 19:02:33 -0400

    Measurement Price Calculator: Round total measurement in cart to 2 decimal places (uses upcoming filter)

--- woocommerce-measurement-price-calculator/change-measurement-precision.php
+++ woocommerce-measurement-price-calculator/change-measurement-precision.php
@@ -1,13 +1,31 @@
 <?php // only copy this line if needed
 
 /**
- * Change the Measurement Price Calculator measurement precision
+ * Change the Measurement Price Calculator measurement precision to 2 decimal
+ * places
  *
  * This controls the number of decimal places used to display the total
- * measurement
+ * measurement on product pages
+ *
+ * @return int The number of decimal places
  */
 function sv_wc_measurement_price_calculator_change_measurement_precision() {
 
 	return 2;
 }
 add_filter( 'wc_measurement_price_calculator_measurement_precision', 'sv_wc_measurement_price_calculator_change_measurement_precision' );
+
+/**
+ * Round the Measurement Price Calculator total measurement on the cart page to
+ * 2 decimal places.
+ *
+ * Requrires v3.10.2+
+ *
+ * @param float $value The total measurement value
+ * @return float The total measurement value rounded to 2 decimal places
+ */
+function sv_wc_measurement_price_calculator_humanize_cart_item_data_total_amount_value( $value ) {
+
+	return round( $value, 2 );
+}
+add_filter( 'wc_measurement_price_calculator_cart_item_data_total_amount_value', 'sv_wc_measurement_price_calculator_humanize_cart_item_data_total_amount_value' );
