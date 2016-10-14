cb5ceb68e2b315b1031aa57773e778e34de3ffa7
Beka Rice <beka.rice@gmail.com>
2016-10-14 14:44:01 -0400

    CSV Export: update item meta (weight) snippet

--- woocommerce-customer-order-csv-export/add-item-meta-to-order-export.php
+++ woocommerce-customer-order-csv-export/add-item-meta-to-order-export.php
@@ -17,40 +17,95 @@
  */
 function sv_wc_csv_export_add_weight_to_line_item( $line_item, $item, $product, $order ) {
 
-	$line_item['weight'] = $product->get_weight();
+	$new_item_data = array();
 
-	return $line_item;
+	foreach ( $line_item as $key => $data ) {
+
+		$new_item_data[ $key ] = $data;
+
+		if ( 'sku' === $key ) {
+			$new_item_data['weight'] = wc_format_decimal( $product->get_weight(), 2 );
+		}
+	}
+
+	return $new_item_data;
 }
 add_filter( 'wc_customer_order_csv_export_order_line_item', 'sv_wc_csv_export_add_weight_to_line_item', 10, 4 );
 
 
 /**
- * Add weight as line item meta in CSV export CSV Import format
+ * Add `item_weight` column to the default and custom formats
  *
- * @param array $order_data the original order data
- * @param object $order the WC_Order object exported
- * @return array the updated order data
+ * @param array $column_headers the original column headers
+ * @param WC_Customer_Order_CSV_Export_Generator $csv_generator the generator instance
+ * @return array - the updated column headers
  */
-function sv_wc_csv_export_add_weight_to_csv_export_import_format( $order_data, $order ) {
+function sv_wc_csv_export_modify_column_headers_item_price( $column_headers, $csv_generator ) {
 
-	$count = 1;
+	$new_headers = array();
 
-	// add line items
-	foreach ( $order->get_items() as $item ) {
+	if ( sv_wc_csv_export_is_one_row( $csv_generator ) ) {
 
-		$product = $order->get_product_from_item( $item );
+		foreach( $column_headers as $key => $column ) {
 
-		if ( ! is_object( $product ) ) {
-			$product = new WC_Product( 0 );
+			$new_headers[ $key ] = $column;
+
+			// add the item_price after the SKU column
+			if ( 'item_sku' === $key ) {
+				$new_headers['item_weight'] = 'item_weight';
+
+			}
 		}
 
-		if ( $weight = $product->get_weight() ) {
-			$order_data[ "order_item_{$count}" ] .= '|weight: ' . $weight;
-		}
+	} else {
 
-		$count++;
+		return $column_headers;
 	}
 
+	return $new_headers;
+}
+add_filter( 'wc_customer_order_csv_export_order_headers', 'sv_wc_csv_export_modify_column_headers_item_price', 10, 2 );
+
+
+/**
+ * Add the item_weight column data for the Default - One Row per Item format
+ *
+ * @param array $order_data the original order data
+ * @param array $item       the item for this row
+ * @return array - the updated order data
+ */
+function sv_wc_csv_export_order_row_one_row_per_item_weight( $order_data, $item ) {
+
+	$order_data['item_weight'] = $item['weight'];
 	return $order_data;
 }
-add_filter( 'wc_customer_order_csv_export_order_row', 'sv_wc_csv_export_add_weight_to_csv_export_import_format', 20, 2 );
+add_filter( 'wc_customer_order_csv_export_order_row_one_row_per_item', 'sv_wc_csv_export_order_row_one_row_per_item_weight', 10, 2 );
+
+
+if ( ! function_exists( 'sv_wc_csv_export_is_one_row' ) ) :
+
+/**
+ * Helper function to check the export format
+ *
+ * @param \WC_Customer_Order_CSV_Export_Generator $csv_generator the generator instance
+ * @return bool - true if this is a one row per item format
+ */
+function sv_wc_csv_export_is_one_row( $csv_generator ) {
+
+	$one_row_per_item = false;
+
+	if ( version_compare( wc_customer_order_csv_export()->get_version(), '4.0.0', '<' ) ) {
+
+		// pre 4.0 compatibility
+		$one_row_per_item = ( 'default_one_row_per_item' === $csv_generator->order_format || 'legacy_one_row_per_item' === $csv_generator->order_format );
+
+	} elseif ( isset( $csv_generator->format_definition ) ) {
+
+		// post 4.0 (requires 4.0.3+)
+		$one_row_per_item = 'item' === $csv_generator->format_definition['row_type'];
+	}
+
+	return $one_row_per_item;
+}
+
+endif;
