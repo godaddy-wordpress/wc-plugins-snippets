32f5d8948d4e790be2f6ac5a911149ddc2209678
Beka Rice <beka.rice@gmail.com>
2016-11-12 17:41:23 -0500

    Memberships: update use of deprecated wp_new_user_notification param

--- woocommerce-memberships/admin/notify-new-users-from-import.php
+++ woocommerce-memberships/admin/notify-new-users-from-import.php
@@ -7,11 +7,17 @@
  * @param string $action Either 'create' or 'merge' (update) a User Membership
  */
 function sv_wc_memberships_import_notify_new_user( $user_membership, $action ) {
+	global $wp_version;
 
 	if ( ! $user_membership->get_user_id() || 'create' !== $action ) {
 		return;
 	}
 
-	wp_new_user_notification( $user_membership->get_user_id(), null, 'user' );
+	// the second param here was deprecated in WP 4.3.1
+	if ( version_compare( $wp_version, '4.3.1', '>=' ) {
+		wp_new_user_notification( $user_membership->get_user_id(), null, 'user' );
+	} else {
+		wp_new_user_notification( $user_membership->get_user_id() );
+	}
 }
 add_action( 'wc_memberships_csv_import_user_membership', 'sv_wc_memberships_import_notify_new_user', 10, 2 );
