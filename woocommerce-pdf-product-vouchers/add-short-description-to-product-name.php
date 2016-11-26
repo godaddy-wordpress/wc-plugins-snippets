b798377c5e2bf0bb235fcb67ddd2e267e2cbf333
Beka Rice <beka.rice@gmail.com>
2016-11-25 19:17:13 -0500

    PDF Vouchers: add short description to product name

---
+++ woocommerce-pdf-product-vouchers/add-short-description-to-product-name.php
@@ -0,0 +1,21 @@
+<?php // only copy if needed
+
+/**
+ * Adds a product short description after the name
+ *
+ * @param string $name the product name
+ * @param \WC_Voucher $voucher voucher instance
+ * @return string updated name
+ */
+function sv_wc_pdf_vouchers_add_product_description( $name, $voucher ) {
+
+	$product = $voucher->get_order()->get_product_from_item( $voucher->get_item() );
+
+	if ( $product && $short_description = $product->get_post_data()->post_excerpt ) {
+		$name .= ' - ' . $short_description;
+	}
+
+	return $name;
+}
+add_filter( 'wc_pdf_product_vouchers_get_product_name', 'sv_wc_pdf_vouchers_add_product_description', 50, 2 );
+
