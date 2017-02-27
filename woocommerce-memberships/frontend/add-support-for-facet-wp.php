6923a4e092c5fa5340dfbb6b1a48a748d86523a1
Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
2017-02-27 10:38:13 -0500

    Add Facet WP compatibility snippet
    Matt from Facet WP suggested not to add this snippet into Facet WP codebase as he thinks the fact wc_user_membership is set as the main query is odd and shouldn't happen -- I'm not sure either why this happens but likely is a side effect of the memberships restrictions mechanism 
    
    this is unlikely to change any time soon and only seems to have disrupted Facet WP -- we can either decide to add the snippet in Memberships or just recommend our customers that use Facet WP to implement it manually

---
+++ woocommerce-memberships/frontend/add-support-for-facet-wp.php
@@ -0,0 +1,19 @@
+<?php // only copy this line if needed
+
+/**
+ * Resolves a compatibility issue between Memberships and Facet WP.
+ *
+ * @param bool $is_main_query
+ * @param \WP_Query $query
+ * @return bool
+ */
+function sv_memberships_facetwp_compatibility( $is_main_query, $query ) {
+
+    if ( 'wc_user_membership' == $query->get( 'post_type' ) ) {
+        $is_main_query = false;
+    }
+	
+    return $is_main_query;
+}
+
+add_filter( 'facetwp_is_main_query', 'sv_memberships_facetwp_compatibility' , 10, 2 );
