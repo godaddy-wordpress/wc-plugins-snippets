6ecf3a987408ca401efc601705ac0a3bf1a53705
Beka Rice <beka.rice@gmail.com>
2017-03-17 02:15:39 -0400

    Memberships: prefer user membership getters over props

--- woocommerce-memberships/admin/trigger-on-memberships-status-change.php
+++ woocommerce-memberships/admin/trigger-on-memberships-status-change.php
@@ -15,12 +15,12 @@
  */
 function sv_wc_memberships_update_user_role_with_membership( $user_membership, $old_status, $new_status ) {
 
-	$user_id = $user_membership->user_id;
+	$user_id = $user_membership->get_user_id();
 	$wp_user = get_userdata( $user_id );
-	$roles = $wp_user->roles;
+	$roles   = $wp_user->roles;
 
 	// Bail if the member doesn't currently have the Site Member role or is an active member
-	if ( ! in_array( 'site_member', $roles ) || wc_memberships_is_user_active_member( $user_id, $user_membership->plan_id ) ) {
+	if ( ! in_array( 'site_member', $roles ) || wc_memberships_is_user_active_member( $user_id, $user_membership->get_plan_id() ) ) {
 		return;
 	}
 
@@ -37,18 +37,18 @@ add_action( 'wc_memberships_user_membership_status_changed', 'sv_wc_memberships_
  * @param string $old_status the previous membership status
  * @param string $new_status the new membership status
  */
-function sv_reactivate_member_role( $user_membership, $old_status, $new_status ) {
+function sv_wc_memberships_reactivate_member_role( $user_membership, $old_status, $new_status ) {
 
-	$user_id = $user_membership->user_id;
+	$user_id = $user_membership->get_user_id();
 	$wp_user = get_userdata( $user_id );
-	$roles = $wp_user->roles;
+	$roles   = $wp_user->roles;
 
 	// Bail if the member currently has the Site Member role, doesn't have the customer role, or is inactive
-	if ( in_array( 'site_member', $roles ) || ! in_array( 'customer', $roles ) || ! wc_memberships_is_user_active_member( $user_id, $user_membership->plan_id ) ) {
+	if ( in_array( 'site_member', $roles ) || ! in_array( 'customer', $roles ) || ! wc_memberships_is_user_active_member( $user_id, $user_membership->get_plan_id() ) ) {
 		return;
 	}
 
 	$wp_user->remove_role( 'customer' );
 	$wp_user->add_role( 'site_member' );
 }
-add_action( 'wc_memberships_user_membership_status_changed', 'sv_reactivate_member_role', 10, 3 );
+add_action( 'wc_memberships_user_membership_status_changed', 'sv_wc_memberships_reactivate_member_role', 10, 3 );
