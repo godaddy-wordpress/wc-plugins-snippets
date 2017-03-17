fbd4aca3c064428a57b89655eeef9aa6133916f0
Beka Rice <beka.rice@gmail.com>
2017-03-17 02:09:20 -0400

    Style: Favor string interpolation v concatenating

--- woocommerce-checkout-add-ons/add-add-on-attributes-like-placeholders.php
+++ woocommerce-checkout-add-ons/add-add-on-attributes-like-placeholders.php
@@ -11,24 +11,25 @@ function sv_wc_checkout_add_ons_add_attributes( $checkout_fields ) {
 	$add_on_id = 3; // change 3 to your add-on id
 
 	// is our checkout add-on a field?
-    if ( isset( $checkout_fields['add_ons'][ 'wc_checkout_add_ons_' . $add_on_id ] ) ) {
-    
+	if ( isset( $checkout_fields['add_ons'][ "wc_checkout_add_ons_{$add_on_id}" ] ) ) {
+
 		// Adds a maximum character length + description to this add on
-        $checkout_fields['add_ons'][ 'wc_checkout_add_ons_' . $add_on_id ]['maxlength']   = "300";
-        $checkout_fields['add_ons'][ 'wc_checkout_add_ons_' . $add_on_id ]['description']   = "Please add no more than 300 characters.";
-    
-    }
+		$checkout_fields['add_ons'][ "wc_checkout_add_ons_{$add_on_id}" ]['maxlength']   = "300";
+		$checkout_fields['add_ons'][ "wc_checkout_add_ons_{$add_on_id}" ]['description'] = "Please add no more than 300 characters.";
+
+	}
 
 	// repeat for each add-on
 	$add_on_id = 7; // change 7 to your add-on id
 
-    if ( isset( $checkout_fields['add_ons'][ 'wc_checkout_add_ons_' . $add_on_id ] ) ) {
-    
+	if ( isset( $checkout_fields['add_ons'][ "wc_checkout_add_ons_{$add_on_id}") ) {
+
 		// Add a maximum length and placeholder
-        $checkout_fields['add_ons'][ 'wc_checkout_add_ons_' . $add_on_id ]['maxlength']   = "140";
-        $checkout_fields['add_ons'][ 'wc_checkout_add_ons_' . $add_on_id ]['placeholder'] = "My custom placeholder";
-        
-    }
+		$checkout_fields['add_ons'][ "wc_checkout_add_ons_{$add_on_id}" ]['maxlength']   = "140";
+		$checkout_fields['add_ons'][ "wc_checkout_add_ons_{$add_on_id}" ]['placeholder'] = "My custom placeholder";
+
+	}
+
 	return $checkout_fields;
 }
 add_filter( 'woocommerce_checkout_fields', 'sv_wc_checkout_add_ons_add_attributes', 20 );
