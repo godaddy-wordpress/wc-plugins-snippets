e8c8acee6595e576dbbb36fec4ab34be3fe8d754
Beka Rice <beka.rice@gmail.com>
2017-11-30 16:39:49 -0500

    GA Pro: don't track imported orders as completed purchases

---
+++ woocommerce-google-analytics-pro/skip-tracking-imported-orders.php
@@ -0,0 +1,24 @@
+<?php // only copy if needed
+
+/**
+ * REQUIRES WOOCOMMERCE 3.0+
+ */
+
+/**
+ * Do not track orders created via Order CSV Import in Google Analytics.
+ *
+ * @param bool $do_not_track true if orders should not be tracked; default: false
+ * @param int $order_id
+ * @return bool
+ */
+function sv_wc_google_analytics_skip_order_tracking( $do_not_track, $order_id ) {
+
+	$order = wc_get_order( $order_id );
+
+	if ( 'csv-import' === $order->get_created_via() ) {
+		$do_not_track = true;
+	}
+
+	return $do_not_track;
+}
+add_filter( 'wc_google_analytics_pro_do_not_track_completed_purchase', '', 10, 2 );
