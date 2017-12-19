4b6d39b98de2b36d4ca656656af6a569ea496f93
Beka Rice <beka.rice@gmail.com>
2017-12-19 02:54:33 -0500

    AuthNet CIM: Try to process captures on order status change

---
+++ woocommerce-gateway-authorize-net-cim/maybe-capture-on-order-status-change.php
@@ -0,0 +1,20 @@
+<?php // only copy if needed
+
+
+/**
+ * Maybe capture an order via AuthNet on status change to processing or completed.
+ *
+ * @param int $order_id the order ID, unused
+ * @param \WC_Order $order order object
+ */
+function sv_wc_authorize_net_cim_maybe_process_capture( $order_id, $order ) {
+
+	// quick check
+	if ( ! $order instanceof WC_Order ) {
+		return;
+	}
+
+	wc_authorize_net_cim()->maybe_capture_charge( $order );
+}
+add_action( 'woocommerce_order_status_processing', 'sv_wc_authorize_net_cim_maybe_process_capture', 10, 2 );
+add_action( 'woocommerce_order_status_completed',  'sv_wc_authorize_net_cim_maybe_process_capture', 10, 2 );
