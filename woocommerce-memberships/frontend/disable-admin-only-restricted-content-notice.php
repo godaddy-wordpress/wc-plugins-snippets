65139e8e73a8d426fd6b3b8d91c71f22a7cb50c7
Tamara Zuk <tamarazuk@gmail.com>
2018-04-11 22:30:18 -0400

    Memberships: Add minimum version required to snippet

--- woocommerce-memberships/frontend/disable-admin-only-restricted-content-notice.php
+++ woocommerce-memberships/frontend/disable-admin-only-restricted-content-notice.php
@@ -2,5 +2,6 @@
 
 /**
  * Disable the notice displayed when an admin is browsing content restricted to non-members.
+ * Minimum v1.10.1 of Memberships required.
  */
 add_filter( 'wc_memberships_display_restricted_content_admin_notice', '__return_false' );
