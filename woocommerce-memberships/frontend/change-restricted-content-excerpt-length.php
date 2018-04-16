4e3447e766f605d90cc481aa5cd38fdae1140541
Tamara Zuk <tamarazuk@gmail.com>
2018-04-15 23:18:15 -0400

    Memberships: Update excerpt length snippet to use new filter in v1.10.1

--- woocommerce-memberships/frontend/change-restricted-content-excerpt-length.php
+++ woocommerce-memberships/frontend/change-restricted-content-excerpt-length.php
@@ -1,33 +1,12 @@
 <?php // only copy this if needed
 
 /**
- * The wc_memberships_the_restricted_content filter allows you to adjust the way
- * restricted content is displayed when a visitor doesn't have access
+ * Changes excerpts displayed on restricted membership content to use 120 words
+ * instead of WordPress default.
  *
- * NOTE: THIS FILTER IS DEPRECATED as of v1.9
- *
- * Use the wc_memberships_{message_code}_html filters instead to adjust restricted content display.
- *
- * With this snippet specifically, the_excerpt filter could replace it.
+ * Requires v1.10.1+ of Memberships
  */
-
-
-/**
- * Example of adjusting restricted content (v1.6.0+)
- * 	Changes excerpts to use 120 words instead of WordPress default
- *
- * @param string $content the content being restricted
- * @param bool $restricted true if the content is restricted for the viewer
- * @param string $message the restricted content notice HTML
- * @param object $post the post object being restricted
- * @return string - the updated restricted content
- */
-function sv_wc_memberships_filter_restricted_content( $content, $restricted, $message, $post ) {
-
-	if ( true === $restricted ) {
-		return wp_trim_words( $content, 120 ) . $message;
-	}
-
-	return $content;
+function sv_wc_memberships_restricted_excerpt_length() {
+	return 120;
 }
-add_filter( 'wc_memberships_the_restricted_content', 'sv_wc_memberships_filter_restricted_content', 10, 4 );
+add_filter( 'wc_memberships_restricted_excerpt_length', 'sv_wc_memberships_restricted_excerpt_length' );
