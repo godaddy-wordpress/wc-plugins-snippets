da0f8ec38bfc121224728bd099575e4ee438773f
Tamara Zuk <tamarazuk@gmail.com>
2018-05-04 01:18:18 -0400

    Tab Manager: Update snippet to avoid fatals when plugin not active

--- woocommerce-tab-manager/remove-tabs-from-site-search.php
+++ woocommerce-tab-manager/remove-tabs-from-site-search.php
@@ -1,5 +1,16 @@
 <?php // only copy if needed
 
+/**
+ * Completely removes product tab content from the site search
+ */
 
-// Completely removes product tab content from the site search
-remove_filter( 'posts_clauses', array( wc_tab_manager()->get_search_instance(), 'modify_search_clauses' ), 20, 2 );
+function sv_wc_tab_manager_disable_tab_search() {
+
+	// bail if Tab Manager isn't active
+	if ( ! function_exists( 'wc_tab_manager' ) ) {
+		return;
+	}
+
+	remove_filter( 'posts_clauses', array( wc_tab_manager()->get_search_instance(), 'modify_search_clauses' ), 20 );
+}
+add_action( 'init', 'sv_wc_tab_manager_disable_tab_search' );
