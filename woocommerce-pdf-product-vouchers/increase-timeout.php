5e4264cc75f8e5aaffba4063fc70b21872302e44
Tamara Zuk <tamarazuk@gmail.com>
2018-05-25 19:25:02 -0400

    PDF Product Vouchers: Increase timeout to 20 since the new default will be 15

--- woocommerce-pdf-product-vouchers/increase-timeout.php
+++ woocommerce-pdf-product-vouchers/increase-timeout.php
@@ -4,6 +4,6 @@
  * Increase the timeout when rendering the voucher HTML to avoid cURL timeout errors.
  */
 function sv_wc_pdf_product_vouchers_render_voucher_html_timeout() {
-	return 10;
+	return 20;
 }
 add_filter( 'wc_pdf_product_vouchers_render_voucher_html_timeout', 'sv_wc_pdf_product_vouchers_render_voucher_html_timeout' );
