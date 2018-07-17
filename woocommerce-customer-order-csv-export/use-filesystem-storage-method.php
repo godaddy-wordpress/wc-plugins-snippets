f54a1ba185fa9e093b877481011cffa970b115b4
Tamara Zuk <tamarazuk@gmail.com>
2018-07-17 17:18:47 -0400

    CSV Export: Use filesystem storage method

---
+++ woocommerce-customer-order-csv-export/use-filesystem-storage-method.php
@@ -0,0 +1,15 @@
+<?php // only copy this line if needed
+
+/**
+ * Use the (legacy) filesystem storage method for Customer/Order CSV Export
+ *
+ * @param array $export_args the arguments being passed in to this export
+ * @return array the modified export arguments
+ */
+function sv_wc_customer_order_csv_export_use_filesystem_storage( $export_args ) {
+
+	$export_args['storage_method'] = 'filesystem';
+
+	return $export_args;
+}
+add_filter( 'wc_customer_order_csv_export_start_export_args', 'sv_wc_customer_order_csv_export_use_filesystem_storage' );
