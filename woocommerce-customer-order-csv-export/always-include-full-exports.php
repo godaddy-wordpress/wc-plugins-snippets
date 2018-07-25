2bf328038802e5bd59b521ca0eaacedc931c08da
Tamara Zuk <tamarazuk@gmail.com>
2018-07-25 01:46:38 -0400

    CSV Export: Update "export all" snippet
    We have purpose-made filters to allow actors to include all orders/customers in auto-exports. The previous version of this required merchants to also mark old orders as not exported, which is a daunting task for large stores.

--- woocommerce-customer-order-csv-export/always-include-full-exports.php
+++ woocommerce-customer-order-csv-export/always-include-full-exports.php
@@ -1,6 +1,7 @@
 <?php // only copy if needed
 
-// Ensures orders and customers are never marked as exported
-// This results in *every* automated export containing full records
-add_filter( 'wc_customer_order_csv_export_mark_order_exported',    '__return_false' );
-add_filter( 'wc_customer_order_csv_export_mark_customer_exported', '__return_false' );
+/**
+ * Ensure all orders and customers are included with all automatic exports
+ */
+add_filter( 'wc_customer_order_csv_export_auto_export_new_orders_only',    '__return_false' );
+add_filter( 'wc_customer_order_csv_export_auto_export_new_customers_only', '__return_false' );
