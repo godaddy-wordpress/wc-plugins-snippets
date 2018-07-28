1f35fc08cb33afa4cecb1acc7cc0213deb63ec96
Tamara Zuk <tamarazuk@gmail.com>
2018-07-27 21:13:05 -0400

    Memberships: Register [woocommerce_login_form] shortcode

---
+++ woocommerce-memberships/wc-login-form-shortcode.php
@@ -0,0 +1,8 @@
+<?php // only copy this line if needed
+
+/**
+ * Registers a `[woocommerce_login_form]` shortcode
+ *
+ * Example usage: [woocommerce_login_form redirect="/my-account/members-area/"]
+ */
+add_shortcode( 'woocommerce_login_form', 'woocommerce_login_form' );
