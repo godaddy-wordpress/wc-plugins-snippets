0831db11224245b6812b43e618fa3617663640a2
Tamara Zuk <tamara@skyverge.com>
2018-10-20 01:13:08 -0400

    COGs: Update snippet to hide costs from all admin locations for non-admins

--- woocommerce-cost-of-goods/hide-line-item-cogs-from-non-admins.php
+++ woocommerce-cost-of-goods/hide-line-item-cogs-from-non-admins.php
@@ -20,5 +20,40 @@ function sv_wc_cog_hide_line_item_cogs_from_non_admins() {
 
 	// remove the order total cost of goods from the order totals section
 	remove_action( 'woocommerce_admin_order_totals_after_total', array( wc_cog()->get_admin_instance()->get_orders_instance(), 'show_order_total_cost' ) );
+
+	// remove cost field from simple products under the 'General' tab
+	remove_action( 'woocommerce_product_options_pricing', array( wc_cog()->get_admin_instance()->get_products_instance(), 'add_cost_field_to_simple_product' ) );
+
+	// remove cost field from variable products under the 'General' tab
+	remove_action( 'woocommerce_product_options_sku', array( wc_cog()->get_admin_instance()->get_products_instance(), 'add_cost_field_to_variable_product' ) );
+
+	// removes the product variation 'Cost' bulk edit action
+	remove_action( 'woocommerce_variable_product_bulk_edit_actions', array( wc_cog()->get_admin_instance()->get_products_instance(), 'add_variable_product_bulk_edit_cost_action' ) );
+
+	// remove cost field from variable products under the 'Variations' tab after the shipping class select
+	remove_action( 'woocommerce_product_after_variable_attributes', array( wc_cog()->get_admin_instance()->get_products_instance(), 'add_cost_field_to_product_variation' ), 15 );
+
+	// remove Products list cost bulk edit field
+	remove_action( 'woocommerce_product_bulk_edit_end', array( wc_cog()->get_admin_instance()->get_products_instance(), 'add_cost_field_bulk_edit' ) );
+
+	// remove Products list quick edit cost field
+	remove_action( 'woocommerce_product_quick_edit_end',  array( wc_cog()->get_admin_instance()->get_products_instance(), 'render_quick_edit_cost_field' ) );
+	remove_action( 'manage_product_posts_custom_column',  array( wc_cog()->get_admin_instance()->get_products_instance(), 'add_quick_edit_inline_values' ) );
+
+	// remove from Bookings products as well
+	if ( wc_cog()->is_plugin_active( 'woocommmerce-bookings.php' ) || wc_cog()->is_plugin_active( 'woocommerce-bookings.php' ) ) {
+
+		// add cost field to booking products under the 'General' tab
+		remove_action( 'woocommerce_product_options_general_product_data', array( wc_cog()->get_admin_instance()->get_products_instance(), 'add_cost_field_to_booking_product' ) );
+	}
+
+	// remove the "Cost" column header next to "Price"
+	remove_filter( 'manage_edit-product_columns', array( wc_cog()->get_admin_instance()->get_products_instance(), 'product_list_table_cost_column_header' ), 11 );
+
+	// removes the product cost in the product list table
+	remove_action( 'manage_product_posts_custom_column', array( wc_cog()->get_admin_instance()->get_products_instance(), 'product_list_table_cost_column' ), 11 );
+
+	// remove reports
+	remove_filter( 'woocommerce_admin_reports', array( wc_cog()->get_admin_reports_instance(), 'add_reports' ) );
 }
 add_action( 'admin_init', 'sv_wc_cog_hide_line_item_cogs_from_non_admins' );
