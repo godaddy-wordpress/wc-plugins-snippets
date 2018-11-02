332fb13999451813bcab0625e704c8b66af918ce
Tamara Zuk <tamara@skyverge.com>
2018-11-01 23:31:29 -0400

    Google Analytics Pro: Move pre-1.5.1 code to new legacy folder to reduce confusion

--- woocommerce-google-analytics-pro/legacy/remove-tracking-code-javascript.php
+++ woocommerce-google-analytics-pro/legacy/remove-tracking-code-javascript.php
@@ -1,14 +1,26 @@
 <?php // only copy this line if needed
 
 /**
- * Remove the tracking code added by WooCommerce Google Analytics Pro (v1.5.1+)
+ * Remove the tracking code added by WooCommerce Google Analytics Pro (pre-1.5.1 only)
  *
  * This is usefull if your theme or another plugin is adding the tracking code
  * and you wish to use that instead
  *
- * Requires at least v1.5.1 of Google Analytics Pro
+ * Note: Only use this code if you're running a version older than v1.5.1
  */
-add_filter( 'wc_google_analytics_pro_remove_tracking_code', '__return_true' );
+function sv_wc_google_analytics_pro_remove_tracking_code() {
+
+	// check if Google Analytics Pro is active
+	if ( ! function_exists( 'wc_google_analytics_pro' ) ) {
+		return;
+	}
+
+	remove_action( 'wp_head',    array( wc_google_analytics_pro()->get_integration(), 'ga_tracking_code' ), 9 );
+	remove_action( 'login_head', array( wc_google_analytics_pro()->get_integration(), 'ga_tracking_code' ), 9 );
+}
+
+add_action( 'init', 'sv_wc_google_analytics_pro_remove_tracking_code' );
+
 
 /**
  * Remove the pageview tracking added by WooCommerce Google Analytics Pro
