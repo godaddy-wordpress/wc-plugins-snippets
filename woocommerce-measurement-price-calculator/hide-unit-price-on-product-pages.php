6f0d14fc3656aa86fdbd4ac9e5b38867f21aa87a
Julie Martin <JulieSkyVerge@users.noreply.github.com>
2018-11-20 12:58:16 -0500

    Update hide unit price

--- woocommerce-measurement-price-calculator/hide-unit-price-on-product-pages.php
+++ woocommerce-measurement-price-calculator/hide-unit-price-on-product-pages.php
@@ -1,22 +1,21 @@
 <?php // only copy this line if needed
-
 /**
  * Hide the unit price from the top of the product page
  * for Measurement Calculator products
  */
 function sv_wc_measurement_price_calculator_hide_unit_price() {
 	global $product;
-
+	
 	// bail if the calculator isn't active or this isn't a product page
 	if ( ! class_exists( 'WC_Price_Calculator_Product' ) || ! is_product() ) {
 		return;
 	}
-
+	
 	$measurement = WC_Price_Calculator_Product::calculator_enabled( $product );
-
+	
 	// if the calculator is enabled, hide unit price
 	if ( $measurement ) {
-		echo '<style>.product form .price { display: none; }</style>';
+		echo '<style> .wc-measurement-price-calculator-price { display: none !important; } </style>';
 	}
 }
 add_action( 'wp_print_footer_scripts', 'sv_wc_measurement_price_calculator_hide_unit_price' );
