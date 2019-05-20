e079ddccfdd99e7ee9d82913ece37c56d737d070
Tamara Zuk <tamara@skyverge.com>
2019-05-20 17:53:13 -0400

    Twilio: Introduce snippet to to move the optin input (plus fix the snippet which removes it to avoid fatal errors)

--- woocommerce-twilio-sms-notifications/move-checkout-optin.php
+++ woocommerce-twilio-sms-notifications/move-checkout-optin.php
@@ -1,8 +1,8 @@
 <?php // only copy if needed
 
 /**
- * Removes Twilio input fields from checkout, allowing it to be used for admin alerts only.
- * Customers will not receive any notifications or be able to opt into them.
+ * Moves Twilio input field from after the billing form to immediately before
+ * the submit button
  */
 
 function sv_wc_twilio_sms_remove_input_fields() {
@@ -12,6 +12,6 @@ function sv_wc_twilio_sms_remove_input_fields() {
 	}
 
 	remove_action( 'woocommerce_after_checkout_billing_form', array( wc_twilio_sms(), 'add_opt_in_checkbox' ) );
-	remove_action( 'woocommerce_checkout_update_order_meta',  array( wc_twilio_sms(), 'process_opt_in_checkbox' ) );
+	add_action( 'woocommerce_review_order_before_submit',  array( wc_twilio_sms(), 'add_opt_in_checkbox' ) );
 }
 add_action( 'init', 'sv_wc_twilio_sms_remove_input_fields' );
