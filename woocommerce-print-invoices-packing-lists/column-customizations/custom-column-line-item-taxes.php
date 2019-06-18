29b63e09c85ba97d7e0678418759888c2511ae71
Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
2019-06-18 15:38:37 +0900

    PIP: List taxes for each invoice item in columns

--- woocommerce-print-invoices-packing-lists/column-customizations/custom-column-line-item-taxes.php
+++ woocommerce-print-invoices-packing-lists/column-customizations/custom-column-line-item-taxes.php
@@ -1,6 +1,5 @@
 <?php // only copy this line if needed
 
-
 /**
  * Filters the document table headers to add a custom column header for line taxes.
  *
@@ -13,12 +12,21 @@ function sv_wc_pip_document_table_headers_add_line_item_taxes( $table_headers, $
 
 	if ( 'invoice' === $document_type ) {
 
-		$new_headers = array();
-
+		$new_headers = $tax_labels = [];
+		$order       = wc_get_order( $order_id );
+				
+		// get the order taxes by ID and label
+		foreach ( $order->get_items( 'tax' ) as $tax_item ) {
+    		$tax_labels[ $tax_item->get_id() ] = $tax_item->get_label();
+		}		
+		
 		foreach ( $table_headers as $key => $name ) {
 
+			// create columns for all taxes before the price column
 			if ( 'price' === $key ) {
-				$new_headers['line_item_tax'] = 'Taxes';
+				foreach ( $tax_labels as $tax_id => $tax_label ) {
+					$new_headers[ 'tax_' . $tax_id ] = $tax_label;	
+				}
 			}
 
 			$new_headers[ $key ] = $name;
@@ -32,7 +40,6 @@ function sv_wc_pip_document_table_headers_add_line_item_taxes( $table_headers, $
 }
 
 add_filter( 'wc_pip_document_table_headers', 'sv_wc_pip_document_table_headers_add_line_item_taxes', 10, 3 );
-
 
 
 /**
@@ -50,14 +57,12 @@ function sv_wc_pip_document_table_row_cells_add_line_item_taxes( $table_row_cell
 
 	if ( 'invoice' === $document_type ) {
 		
-		$tax_items_labels = [];
+		$new_row_cells = $tax_labels = [];
 		
 		// get the order taxes by ID and label
 		foreach ( $order->get_items( 'tax' ) as $tax_item ) {
-    		$tax_items_labels[ $tax_item->get_rate_id() ] = $tax_item->get_label();
+    		$tax_labels[ $tax_item->get_rate_id() ] = $tax_item->get_label();
 		}		
-
-		$new_row_cells = [];
 
 		foreach ( $table_row_cells as $key => $value ) {
 
@@ -66,29 +71,14 @@ function sv_wc_pip_document_table_row_cells_add_line_item_taxes( $table_row_cell
 				$taxes = $item->get_taxes();
 				$taxes = isset( $taxes['subtotal'] ) ? $taxes['subtotal'] : [];
 				
-				if ( ! empty( $taxes ) ) {
-					
-					$line_item_taxes = '';
-				
-					// list all item taxes 
-					foreach( $taxes as $tax_id => $tax_amt ) {
-						if ( isset( $tax_items_labels[ $tax_id ] ) ) {
-							$line_item_taxes .= '<dt>' . $tax_items_labels[ $tax_id ] . ':</dt> <dd>' . wc_price( $tax_amt ) . '</dd>'; 
-						}
-					}
-					
-					if ( empty( $line_item_taxes ) ) {
-						$line_item_tax = wc_price( $item->get_total_tax() );	
+				// populate row cells with the item's tax amounts
+				foreach ( $tax_labels as $tax_id => $tax_label ) {
+					if ( isset( $taxes[ $tax_id ] ) ) {
+						$new_row_cells[ 'tax_' . $tax_id ] =  '<span class="price">' . wc_price( $taxes[ $tax_id ] ) . '</span>';
 					} else {
-						$line_item_tax = '<dl>' . $line_item_taxes . '</dl>';
-					}					
-					
-				} else {
-				
-					$line_item_tax = wc_price( $item->get_total_tax() );	
+						$new_row_cells[ 'tax_' . $tax_id ] = '';
+					}
 				}
-				
-				$new_row_cells['line_item_tax'] =  '<span class="price">' . $line_item_tax . '</span>';
 			}
 
 			$new_row_cells[ $key ] = $value;
