767952c69bd4fc52f7addc4bd072e3fdbbdfd174
Kristy Burge <kristy@skyverge.com>
2019-11-29 13:15:50 -0600

    Memberships: Ensure processing access is removed by changing the priority

--- woocommerce-memberships/admin/only-grant-access-for-completed-orders.php
+++ woocommerce-memberships/admin/only-grant-access-for-completed-orders.php
@@ -8,7 +8,7 @@ function sv_wc_memberships_remove_processing_access() {
 
 	// make sure Memberships is active first
 	if ( function_exists( 'wc_memberships' ) ) {
-		remove_action( 'woocommerce_order_status_processing', array( wc_memberships()->get_plans_instance(), 'grant_access_to_membership_from_order' ), 11 );
+		remove_action( 'woocommerce_order_status_processing', array( wc_memberships()->get_plans_instance(), 'grant_access_to_membership_from_order' ), 9 );
 	}
 }
 add_action( 'init', 'sv_wc_memberships_remove_processing_access' );
