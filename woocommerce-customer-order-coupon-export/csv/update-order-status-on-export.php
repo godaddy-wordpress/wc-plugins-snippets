d794ba1e7e9cf1efc83202d3ac5f6d25541f2c8c
Simon Porter <simonporter007@users.noreply.github.com>
2019-12-12 20:45:16 +0000

    fix incorrect arguments

--- woocommerce-customer-order-coupon-export/csv/update-order-status-on-export.php
+++ woocommerce-customer-order-coupon-export/csv/update-order-status-on-export.php
@@ -3,12 +3,12 @@
 /**
  * Update an order's status to completed immediately after it's exported
  *
- * @param bool $mark_as_exported whether to mark the order as exported; defaults to true
  * @param \WC_Order $order order being exported
  * @param string $method how the order is exported (ftp, download, etc)
+ * @param string|null $order_note order note message, null if no note was added
  * @param \WC_Customer_Order_CSV_Export_Handler $handler handler instance
  */
-function sv_wc_csv_export_update_exported_order_status( $mark_as_exported, $order, $export_method, $handler ) {
+function sv_wc_csv_export_update_exported_order_status( $order, $export_method, $order_note, $handler ) {
 
 	// uncomment this to avoid setting the order status to complete for a certain export method
 	// if ( 'ftp' !== $export_method ) {
@@ -27,7 +27,6 @@ add_action( 'wc_customer_order_export_csv_order_exported', 'sv_wc_csv_export_upd
  * Update an order's status to completed upon export only if it's paid
  * Requires WC 2.5+
  *
- * @param bool $mark_as_exported whether to mark the order as exported; defaults to true
  * @param \WC_Order $order order being exported
  */
 function sv_wc_csv_export_update_exported_order_status_on_paid( $order ) {
@@ -36,4 +35,4 @@ function sv_wc_csv_export_update_exported_order_status_on_paid( $order ) {
 		$order->update_status( 'completed', 'Order exported to CSV and set to completed.' );
 	}
 }
-add_action( 'wc_customer_order_export_csv_order_exported', 'sv_wc_csv_export_update_exported_order_status_on_paid', 10, 2 );
+add_action( 'wc_customer_order_export_csv_order_exported', 'sv_wc_csv_export_update_exported_order_status_on_paid', 10, 1 );
