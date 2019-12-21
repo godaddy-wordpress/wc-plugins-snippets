26a1d1b24ee8cad64e95e4ab2bde9835f0c66c07
Tamara Zuk <tamara@skyverge.com>
2019-12-20 23:00:57 -0500

    Memberships: Update snippet to only grant access to Completed order when using Teams as well

--- woocommerce-memberships/admin/only-grant-access-for-completed-orders.php
+++ woocommerce-memberships/admin/only-grant-access-for-completed-orders.php
@@ -8,7 +8,14 @@ function sv_wc_memberships_remove_processing_access() {
 
 	// make sure Memberships is active first
 	if ( function_exists( 'wc_memberships' ) ) {
+
 		remove_action( 'woocommerce_order_status_processing', array( wc_memberships()->get_plans_instance(), 'grant_access_to_membership_from_order' ), 9 );
+	}
+
+	// Add support for Teams for Memberships but make sure it is active first
+	if ( function_exists( 'wc_memberships_for_teams' ) ) {
+
+		remove_action( 'woocommerce_order_status_processing', array( wc_memberships_for_teams()->get_orders_instance(), 'process_team_actions_for_order' ), 10 );
 	}
 }
 add_action( 'init', 'sv_wc_memberships_remove_processing_access' );
