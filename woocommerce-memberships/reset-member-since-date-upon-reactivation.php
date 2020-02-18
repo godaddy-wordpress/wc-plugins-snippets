cbf1ec1e4c23845b4ffdc7053a44dba0379af8b1
Beka Rice <beka.rice@gmail.com>
2020-02-18 15:00:01 -0500

    Memberships: fix code styles

--- woocommerce-memberships/reset-member-since-date-upon-reactivation.php
+++ woocommerce-memberships/reset-member-since-date-upon-reactivation.php
@@ -1,5 +1,6 @@
 <?php // only copy this line if needed!
 
+
 /**
  * Update cancelled membership start date to now.
  *
@@ -11,10 +12,14 @@
  * }
  */
 add_action( 'wc_memberships_user_membership_created', function( $plan, $details ) {
+
 	$membership = wc_memberships_get_user_membership( $details['user_membership_id'] );
+
 	// if we're updating something that was cancelled before...
 	if ( $details['is_update'] && $membership->get_cancelled_date() ) {
+
 		// set start date to now
 		$membership->set_start_date();
 	}
+
 }, 10, 2 );
