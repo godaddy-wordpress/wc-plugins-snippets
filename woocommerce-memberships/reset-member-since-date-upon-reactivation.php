57e827ab17734c835b6382e36c6a52b0289be428
Lindsey Fogle <42387570+lgfogle@users.noreply.github.com>
2020-02-18 13:14:04 -0500

    Memberships: Reset "Member Since" upon reactivation
    When a cancelled membership is reactivated, reset "Member Since" to the current date. This helps to ensure that members don't gain access to content that dripped when their membership was cancelled. Props @bekarice

---
+++ woocommerce-memberships/reset-member-since-date-upon-reactivation.php
@@ -0,0 +1,20 @@
+<?php // only copy this line if needed!
+
+/**
+ * Update cancelled membership start date to now.
+ *
+ * @param \WC_Memberships_Membership_Plan $membership_plan the plan that user was granted access to
+ * @param array $args array of User Membership arguments {
+ *     @type int $user_id the user ID the membership is assigned to
+ *     @type int $user_membership_id the user membership ID being saved
+ *     @type bool $is_update whether this is a post update or a newly created membership
+ * }
+ */
+add_action( 'wc_memberships_user_membership_created', function( $plan, $details ) {
+	$membership = wc_memberships_get_user_membership( $details['user_membership_id'] );
+	// if we're updating something that was cancelled before...
+	if ( $details['is_update'] && $membership->get_cancelled_date() ) {
+		// set start date to now
+		$membership->set_start_date();
+	}
+}, 10, 2 );
