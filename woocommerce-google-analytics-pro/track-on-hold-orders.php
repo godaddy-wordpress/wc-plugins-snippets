abb6bef6002a6b42af3ee110e999fca3e71ff1d2
Simon Porter <simonporter007@users.noreply.github.com>
2020-02-19 13:31:02 +0000

    Google Analytics Pro: mark on-hold orders as a paid status for tracking

--- woocommerce-google-analytics-pro/track-on-hold-orders.php
+++ woocommerce-google-analytics-pro/track-on-hold-orders.php
@@ -1,6 +1,16 @@
 <?php // only copy this line if needed
 
 /**
+ * Marks on-hold orders as a paid status, so that the `completed_purchase` event can track them
+ */
+function sv_wc_google_analytics_pro_woocommerce_order_is_paid_statuses( $paid_statuses ) {
+
+	$paid_statuses[] = 'on-hold';
+
+	return $paid_statuses;
+}
+
+/**
  * Ensures orders marked as On-hold are also tracked as purchases
  */
 function sv_wc_google_analytics_pro_track_on_hold_orders() {
@@ -10,6 +20,7 @@ function sv_wc_google_analytics_pro_track_on_hold_orders() {
 		return;
 	}
 
+	add_filter( 'woocommerce_order_is_paid_statuses', 'sv_wc_google_analytics_pro_woocommerce_order_is_paid_statuses' );
 	add_action( 'woocommerce_order_status_on-hold', array( wc_google_analytics_pro()->get_integration(), 'completed_purchase' ) );
 }
 add_action( 'init', 'sv_wc_google_analytics_pro_track_on_hold_orders' );
