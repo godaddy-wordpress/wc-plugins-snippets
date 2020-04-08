c6adf64b76536639bca48fcb0a01b53d12128d0a
Tamara Zuk <tamara@skyverge.com>
2020-04-07 21:41:06 -0400

    Local Pickup Plus: Add additional pickup appointment durations to the settings

---
+++ woocommerce-shipping-local-pickup-plus/add-additional-appointment-durations.php
@@ -0,0 +1,20 @@
+<?php // only copy this line if needed
+
+/**
+ * Add additional pickup appointment durations to the Local Pickup Plus settings.
+ *
+ * @param array $form_fields settings fields
+ * @return array the updated settings fields
+ */
+function sv_wc_local_pickup_plus_add_duration_settings( $form_fields ) {
+
+	$addition_durations = [
+		5  * MINUTE_IN_SECONDS => __( '5 minutes', 'woocommerce-shipping-local-pickup-plus' ),
+		10 * MINUTE_IN_SECONDS => __( '10 minutes', 'woocommerce-shipping-local-pickup-plus' ),
+	];
+
+	$form_fields['default_appointment_duration']['options'] = array_merge( $addition_durations, $form_fields['default_appointment_duration']['options'] );
+
+	return $form_fields;
+}
+add_filter( 'wc_local_pickup_plus_settings', 'sv_wc_local_pickup_plus_add_duration_settings' );
