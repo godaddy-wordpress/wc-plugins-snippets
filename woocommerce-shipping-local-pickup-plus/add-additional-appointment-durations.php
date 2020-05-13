80214714fe822d7e1e97474337de158fe4c9ad5d
Tamara Zuk <tamara@skyverge.com>
2020-05-13 02:57:08 -0400

    Local Pickup Plus: Add a 2 hour appointment time duration

--- woocommerce-shipping-local-pickup-plus/add-additional-appointment-durations.php
+++ woocommerce-shipping-local-pickup-plus/add-additional-appointment-durations.php
@@ -9,8 +9,9 @@
 function sv_wc_local_pickup_plus_add_duration_settings( $form_fields ) {
 
 	$addition_durations = [
-		5  * MINUTE_IN_SECONDS => __( '5 minutes', 'woocommerce-shipping-local-pickup-plus' ),
-		10 * MINUTE_IN_SECONDS => __( '10 minutes', 'woocommerce-shipping-local-pickup-plus' ),
+		5  * MINUTE_IN_SECONDS  => __( '5 minutes', 'woocommerce-shipping-local-pickup-plus' ),
+		10 * MINUTE_IN_SECONDS  => __( '10 minutes', 'woocommerce-shipping-local-pickup-plus' ),
+		120 * MINUTE_IN_SECONDS => __( '2 hours', 'woocommerce-shipping-local-pickup-plus' ),
 	];
 
 	$form_fields['default_appointment_duration']['options'] = array_merge( $addition_durations, $form_fields['default_appointment_duration']['options'] );
