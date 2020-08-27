99f0b00d4a1cc1e04270de6ac887bc66a9ba4397
Lindsey <lindsey@skyverge.com>
2020-08-27 11:32:38 -0400

    Add snippet to remove dashboard

---
+++ skyverge-dashboard/remove-skyverge-dashboard.php
@@ -0,0 +1,5 @@
+<?php // only use if needed!
+// remove SkyVerge support dashboard
+add_action( 'admin_menu', function() { remove_menu_page( 'skyverge' ); }, 99 );
+// remove dashboard stylesheet
+add_action( 'admin_enqueue_scripts', function() { wp_dequeue_style( 'sv-wordpress-plugin-admin-menus' ); }, 20 );
