f95d2679fa7c4a885ffebb4cd29f4fefb20e17e3
Christine <cxpdev@outlook.com>
2020-10-02 19:11:22 +0200

    Update and rename invoice to documents

--- woocommerce-print-invoices-packing-lists/remove-purchase-notes-on-printed-docs.php
+++ woocommerce-print-invoices-packing-lists/remove-purchase-notes-on-printed-docs.php
@@ -1,7 +1,7 @@
 <?php // only copy this line if needed
 
 /**
- * Remove WooCommerce's Product Purchase Note from the printed invoice. 
+ * Remove WooCommerce's Product Purchase Note from printed documents. 
  */
 
 add_filter( 'wc_pip_order_item_meta_show_purchase_note', '__return_false' );
