4d657bf5c7861ca86bf062df73b20ce0961c111b
Marcus Burnette <marcusdburnette@gmail.com>
2020-10-06 08:49:22 -0400

    FB: added note to interval snippet for resetting scheduled actions

--- facebook-for-woocommerce/change-feed-file-generation-interval.php
+++ facebook-for-woocommerce/change-feed-file-generation-interval.php
@@ -3,6 +3,10 @@
 /**
  * Change feed file generation interval
  * ex: every 24 hours
+ *
+ * NOTE: to reset schedule fully, Product Sync must be
+ * disabled, saved, and re-enabled to allow the new interval
+ * settings to take effect
  */
  
  add_filter( 'wc_facebook_feed_generation_interval', function(){ return HOUR_IN_SECONDS * 24; } );
