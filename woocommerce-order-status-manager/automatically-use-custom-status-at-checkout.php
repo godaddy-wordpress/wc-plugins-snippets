9d0c3571028034fb63bc1b6d9853df1bc634ea75
Erica Ross <erica3738@godaddy.com>
2021-08-13 17:36:53 -0500

    Create automatically-use-custom-status-at-checkout.php

---
+++ woocommerce-order-status-manager/automatically-use-custom-status-at-checkout.php
@@ -0,0 +1,12 @@
+<?php // only copy this line if needed
+/**
+ * Order status will automatically change to the custom status upon checkout.
+ *
+ * @param string $status_slug the slug for the order status
+ * @return string a paid status of order completed
+ */
+
+function sv_wc_order_status_manager_payment_complete_order_status( $status_slug ) {
+	return 'custom-status-slug'; // Change to the slug for your custom status
+}
+add_action( 'woocommerce_payment_complete_order_status', 'sv_wc_order_status_manager_payment_complete_order_status' );
